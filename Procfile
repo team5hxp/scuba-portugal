7105da563530f6d3806273c248d44fb939c04c73
Bryan Beecham <bryan_beecham@hotmail.com>
2017-06-29 10:38:27 -0400

    putting everything in gettingstarted

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn hello.wsgi --log-file -
+web: gunicorn gettingstarted.wsgi --log-file -
--- hello/urls.py
+++ hello/urls.py
@@ -3,14 +3,14 @@ from django.conf.urls import include, url
 from django.contrib import admin
 admin.autodiscover()
 
-import hello.views
+import gettingstarted.views
 
 # Examples:
 # url(r'^$', 'gettingstarted.views.home', name='home'),
 # url(r'^blog/', include('blog.urls')),
 
 urlpatterns = [
-    url(r'^$', hello.views.index, name='index'),
-    url(r'^db', hello.views.db, name='db'),
+    url(r'^$', gettingstarted.views.index, name='index'),
+    url(r'^db', gettingstarted.views.db, name='db'),
     url(r'^admin/', include(admin.site.urls)),
 ]
