93984fda2666602417abe7d829f444526969c3ae
Bryan Beecham <bryan_beecham@hotmail.com>
2017-09-08 13:50:54 -0400

    creating template

---
+++ tests/features/home.py
@@ -0,0 +1,25 @@
+from lettuce import step, world
+
+
+@step("the page is loaded")
+def step_impl(step):
+    """
+    :type step: lettuce.core.Step
+    """
+    pass
+
+
+@step("I am on the home page")
+def step_impl(step):
+    """
+    :type step: lettuce.core.Step
+    """
+    pass
+
+
+@step("I expect to see the proper group name in the title")
+def step_impl(step):
+    """
+    :type step: lettuce.core.Step
+    """
+    pass
